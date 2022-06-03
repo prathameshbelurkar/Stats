6f7ad28197ddb51e32ad19d759e083950ec14421
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-03 11:39:26 +0530

    typescript: inheritance and composition

--- build/index.js
+++ build/index.js
@@ -1,21 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const reader = new CsvFileReader_1.CsvFileReader("football.csv");
-reader.read();
-// enum - enumeration
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HomeWin"] = "H";
-    MatchResult["AwayWin"] = "A";
-    MatchResult["Draw"] = "D";
-})(MatchResult || (MatchResult = {}));
+const MatchResult_1 = require("./MatchResult");
+//  Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
-    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+for (let match of matchReader.matches) {
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
