770e1f0d4e6c8f2c83d5345b50b26e35d1140697
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-04 14:23:54 +0530

    add static methods and generate report

--- build/index.js
+++ build/index.js
@@ -1,20 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const CsvFileReader_1 = require("./CsvFileReader");
-const MatchResult_1 = require("./MatchResult");
-//  Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
-// Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const Summary_1 = require("./Summary");
+const matchReader = MatchReader_1.MatchReader.fromCsv("football.csv");
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Man United");
 matchReader.load();
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
